2d39536554952b55762aae9c454c576d27d83b1e
Villagechief <478237917@qq.com>
2023-08-28 20:36:37 +0800

    【描述】 创建spring alibaba的项目结构 【节奏】

---
+++ src/main/java/com/village/wanchongshan/WanchongshanApplication.java
@@ -0,0 +1,13 @@
+package com.village.wanchongshan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanchongshanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WanchongshanApplication.class, args);
+    }
+
+}
